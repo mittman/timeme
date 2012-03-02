3a53ccf199108403e1496de5f3dc7f4361286d5a
mittman <mittman@users.noreply.github.com>
2012-03-01 20:00:35 -0600

    testing from vbox

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("This is a test");
 		
 		GUI home = new GUI();
 	    home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
