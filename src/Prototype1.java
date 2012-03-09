b76db4b8c04b639afe6186b560394a60b140b564
mittman <mittman@users.noreply.github.com>
2012-03-09 04:26:35 -0800

    Improved prototype

--- src/Prototype1.java
+++ src/Prototype1.java
@@ -22,16 +22,17 @@ public class Prototype1 extends JPanel {
          
         JPanel tab1 = new JPanel();
         tab1.add(eventTab());
-        tab1.add(eventTab());
+        tab1.add(eventDock());
     	tabbedPane.addTab("<html><body topmargin=8 marginheight=10>Add Event</body></html>", tab1);
         
         JPanel tab2 = new JPanel();
         tab2.add(manageTab());
-        tab2.add(manageTab());
+        tab2.add(manageDock());
     	tabbedPane.addTab("<html><body topmargin=8 marginheight=10>Manage Events</body></html>", tab2);
   
         JPanel tab3 = new JPanel();
         tab3.add(summaryTab());
+        tab3.add(summaryDock());
     	tabbedPane.addTab("<html><body topmargin=8 marginheight=10>Summary</body></html>", tab3);
         
         
@@ -50,6 +51,28 @@ public class Prototype1 extends JPanel {
         timer.setFont(new Font(timer.getFont().getName(),timer.getFont().getStyle(),30));
         head.add(timer);
         return head;
+    }
+    
+    protected JPanel eventDock()
+    {
+        JPanel pane = new JPanel();
+        
+        JComponent buttons = new JPanel();
+        Dimension dock = new Dimension(200,250);
+        buttons.setMaximumSize(dock);
+        buttons.setPreferredSize(dock);
+        buttons.setMinimumSize(dock);
+        
+        JButton button1 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>START</body></html>");
+        button1.setAlignmentX(CENTER_ALIGNMENT);
+        JButton button2 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>STOP</body></html>");
+        button2.setAlignmentX(CENTER_ALIGNMENT);
+        buttons.add(button1);
+        buttons.add(button2);
+                
+        pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
+        pane.add(buttons);
+        return pane;
     }
     
     protected JPanel eventTab()
@@ -85,13 +108,38 @@ public class Prototype1 extends JPanel {
         return pane;
     }
     
-    protected JPanel manageTab()
+    protected JPanel manageDock()
     {
         JPanel pane = new JPanel();
         
- 
+        JComponent buttons = new JPanel();
+        Dimension dock = new Dimension(200,250);
+        buttons.setMaximumSize(dock);
+        buttons.setPreferredSize(dock);
+        buttons.setMinimumSize(dock);
+        
+        JButton button1 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>RESUME</body></html>");
+        button1.setAlignmentX(CENTER_ALIGNMENT);
+        JButton button2 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>EDIT</body></html>");
+        button2.setAlignmentX(CENTER_ALIGNMENT);
+        JButton button3 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>DELETE</body></html>");
+        button3.setAlignmentX(CENTER_ALIGNMENT);
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+                
+
+        pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
+        pane.add(buttons);
+        return pane;
+    }
+      
+    protected JPanel manageTab()
+    {
+        JPanel pane = new JPanel();
+    	
         JComponent component = new JPanel();
-        Dimension size = new Dimension(150,100);
+        Dimension size = new Dimension(100,100);
         component.setMaximumSize(size);
         component.setPreferredSize(size);
         component.setMinimumSize(size);
@@ -102,22 +150,38 @@ public class Prototype1 extends JPanel {
                                   TitledBorder.BELOW_TOP);
         border.setTitleColor(Color.red);
         component.setBorder(border);
- 
-        JButton button = new JButton("This is a JButton");
-        
+         
         String title;
-
         title = "Manage";
-        button.setAlignmentX(CENTER_ALIGNMENT);
 
- 
         pane.setBorder(BorderFactory.createTitledBorder(title));
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        pane.add(button);
         pane.add(component);
         return pane;
+
     }
-      
+
+    protected JPanel summaryDock()
+    {
+        JPanel pane = new JPanel();
+        
+        JComponent buttons = new JPanel();
+        Dimension dock = new Dimension(200,250);
+        buttons.setMaximumSize(dock);
+        buttons.setPreferredSize(dock);
+        buttons.setMinimumSize(dock);
+        
+        JButton button1 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>BROWSE</body></html>");
+        button1.setAlignmentX(CENTER_ALIGNMENT);
+        JButton button2 = new JButton("<html><body leftmargin=20 topmargin=8 marginwidth=30 marginheight=20>GENERATE</body></html>");
+        button2.setAlignmentX(CENTER_ALIGNMENT);
+        buttons.add(button1);
+        buttons.add(button2);
+                
+        pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
+        pane.add(buttons);
+        return pane;
+    }
+    
     protected JPanel summaryTab()
     {
         JPanel pane = new JPanel();
